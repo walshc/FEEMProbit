7eea5de1094203e44e092dc31151e693f6bca4ec
Christoph Walsh <criostoirbreathnach@gmail.com>
2016-09-07 20:21:14 -0400

    first commit

---
+++ R/FEEMProbit.R
@@ -0,0 +1,76 @@
+FEEMProbit <- function(formula, data, tol = 1e-9, show.progress = FALSE) {
+
+  cl <- match.call()
+
+  # Check if options were used correctly:
+  if (!("pdata.frame" %in% class(data))) {
+    stop("'data' must be a 'pdata.frame'. Use package 'plm'.")
+  }
+
+  if (!(is.numeric(tol))) {
+    stop("'tol' must be numeric. Default is 1e-9.")
+  }
+
+  # Check if data is balanced and if not balance it:
+  if (!plm::pdim(data)$balanced) {
+    un.id <- sort(unique(index(data, "id")))
+    un.time <- sort(unique(index(data, "time")))
+    rownames(data) <- paste(index(data, "id"), index(data, "time"), sep = ".")
+    allRows <- as.character(t(outer(un.id, un.time, paste, sep = ".")))
+    data <- data[allRows, ]
+    rownames(data) <- allRows
+    index <- data.frame(id = rep(un.id, each = length(un.time)),
+                        time = rep(un.time, length(un.id)),
+                        row.names = rownames(data))
+    class(index) <- c("pindex", "data.frame")
+    attr(data, "index") <- index
+    msg <- paste("This function doesn't work with unbalanced panel data.",
+                 "Forcing to be balanced")
+    warning(msg)
+  }
+
+  N <- length(unique(index(data, "id")))
+  nT <- length(unique(index(data, "time")))
+  mf <- model.frame(formula, data)
+  y <- as.matrix(mf[[1]])
+  X <- as.matrix(mf[, c(2:ncol(mf))])
+
+  # Functions to use inside E-M algorithm:
+  get_mu_k <- function(X, beta_k, alpha_k) {
+    X %*% beta_k + rep(alpha_k, each = nT)
+  }
+  get_y_k <- function(y, mu_k) {
+    p <- pnorm(mu_k)
+    mu_k + ((y - p) * dnorm(mu_k)) / (p * (1 - p))
+  }
+  get_beta_k <- function(y_k, alpha_k) {
+    solve(t(X) %*% X) %*% (t(X) %*% (y_k - rep(alpha_k, each = nT)))
+  }
+  get_alpha_k <- function(y_k, beta_k) {
+    df <- data.table(a = c(y_k - X %*% beta_k), id = rep(1:N, each = nT))
+    df[, mean(a), by = id]$V1
+  }
+
+  # Run the E-M algorithm:
+  dist <- tol + 1
+  beta_k <- matrix(rep(0, ncol(X)))
+  alpha_k <- matrix(rep(0, N))
+  while (dist > tol) {
+    # E-step:
+    mu_k <- get_mu_k(X, beta_k, alpha_k)
+    y_k <- get_y_k(y, mu_k)
+    # M-step:
+    new_beta_k <- get_beta_k(y_k, alpha_k)
+    new_alpha_k <- get_alpha_k(y_k, beta_k)
+    dist <- norm(as.matrix(c(new_beta_k, new_alpha_k) - c(beta_k, new_alpha_k)))
+    beta_k <- new_beta_k
+    alpha_k <- new_alpha_k
+    if (show.progress) {
+      print(dist)
+    }
+  }
+  beta_k <- c(beta_k)
+  names(beta_k) <- names(mf)[2:ncol(mf)]
+  names(alpha_k) <- sort(unique(index(data, "id")))
+  return(list(call = cl, coefficients = beta_k, fixed.effects = alpha_k))
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# FEEMProbit
+An `R` package with a function to estimate the individual fixed effects expecation-maximization estimator in [Chen (2016)](http://blogs.bu.edu/mlchen/files/2016/03/JMP-March10th2016-version.pdf). This is work in progress and might not work as expected.
+
+## Installation
+
+```r
+if (!require(devtools)) install.packages("devtools")
+devtools::install_github("walshc/FEEMProbit")
+```
+
+## Example Usage
+```r
+FEEMProbit(y ~ x1 + x2, data = df)
+```
